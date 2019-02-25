485c3cbe728e1c082de96f736fabb765dd87863b
Alejandro Soto <equistres@gmail.com>
2019-02-25 12:00:24 -0300

    Updates

---
+++ precache-manifest.f0207e8555abea6d281c51d91875b0a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "976c8f8f3f76f6e89219",
+    "url": "/starwars/static/css/main.2e1b27ad.chunk.css"
+  },
+  {
+    "revision": "976c8f8f3f76f6e89219",
+    "url": "/starwars/static/js/main.976c8f8f.chunk.js"
+  },
+  {
+    "revision": "95e65c6947d44018319e",
+    "url": "/starwars/static/js/runtime~main.95e65c69.js"
+  },
+  {
+    "revision": "838835256c241f9b6b64",
+    "url": "/starwars/static/js/2.83883525.chunk.js"
+  },
+  {
+    "revision": "eeecfb4866cb83c610f0f29400f541ad",
+    "url": "/starwars/static/media/logo.eeecfb48.png"
+  },
+  {
+    "revision": "175399071c324ec4406ac4e9d2f495b2",
+    "url": "/starwars/index.html"
+  }
+];
